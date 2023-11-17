3dfdd5cebed0167fc2a7cc9859e8057239b5a39d
YellyWard <148737185+YellyWard@users.noreply.github.com>
2023-11-17 12:05:21 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,14 @@
+
+<?php
+// Database connection configuration
+$connection = mysqli_connect('localhost', 'root', '', 'db_softeng');
+ 
+ 
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL Server".
+    mysqli_connect_errno();  
+} else{
+    echo "John Howard Aguilar ";
+}
+ 
+?>
